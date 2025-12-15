57d192670f8270594e6226ecd5ed25f53902f09c
sykikxo <xo.sykik@gmail.com>
2025-12-15 08:22:56 +0530

    ollama integration fixes. changed prompt

--- ollama_integration.py
+++ ollama_integration.py
@@ -2,6 +2,6 @@ import ollama
 
 def ollama_integration(body, subject, sender):
     response = ollama.chat(model='sum', messages=[
-        {'role': 'user', 'content': f"without any greeting, summarize the email in the least amount of characters.\n subject: {subject}\n sender: {sender}\n body: {body}"}
+        {'role': 'user', 'content': f"sender: {sender}\nsubject: {subject}\nbody: {body}"}
     ])
     return response['message']['content']
