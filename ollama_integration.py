f1040bca08066b6ad128238f9fa2ad4333b709d2
sykikxo <xo.sykik@gmail.com>
2025-12-15 07:56:41 +0530

    ollama integration fixes. switched to custom model

--- ollama_integration.py
+++ ollama_integration.py
@@ -1,7 +1,7 @@
 import ollama
 
 def ollama_integration(body, subject, sender):
-    response = ollama.chat(model='gemma3:1b-it-fp16', messages=[
-        {'role': 'user', 'content': f"without any greeting and using numbers and bullet points summarize the email in the least amount of characters.\n subject: {subject}\n sender: {sender}\n body: {body}"}
+    response = ollama.chat(model='sum', messages=[
+        {'role': 'user', 'content': f"without any greeting, summarize the email in the least amount of characters.\n subject: {subject}\n sender: {sender}\n body: {body}"}
     ])
     return response['message']['content']
